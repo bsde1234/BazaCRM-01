c42b5037395fa58a05d1427625211108ffeb2d76
Artem Prokopenko <Sundey@i.ua>
2019-05-21 00:25:46 +0300

    Add Images

--- src/components/body/addOffer/addImages.js
+++ src/components/body/addOffer/addImages.js
@@ -6,32 +6,55 @@ export default class AddImages extends Component {
 
     this.state = ({
       uid: this.props.user.uid,
-      files:[]
+      files: [],
+      error: ''
     });
-    this.handleFiles=this.handleFiles.bind(this);
+    this.handleFiles = this.handleFiles.bind(this);
   }
   addPhoto() {
     document.getElementById('file').click();
   }
   handleFiles(event) {
-    if (event.target.files.length > 0 /*&& event.target.files["0"].size < 5000000 */) {
-      for(let i = 0; i<event.target.files.length; i++){
-
-        const img = document.createElement("img");
-        img.src = window.URL.createObjectURL(event.target.files[i]);
-        img.onload = function () {
-          window.URL.revokeObjectURL(this.src);
+    if (event.target.files.length > 0) {
+      for (let i = 0; i < event.target.files.length; i++) {
+        if (i <= 4) {
+          if (event.target.files[i].size < 5000000) {
+            const img = document.createElement("img");
+            img.src = window.URL.createObjectURL(event.target.files[i]);
+            this.setState(prevState => ({
+              files: [...prevState.files, img.src],
+              error: ''
+            }));
+          } else {
+            this.setState({
+              error: 'Максимальное количество изображений.'
+            })
+          }
+        } else {
+          this.setState({
+            error: 'Размер изображения не должен привышать 5 МБ.'
+          })
         }
-        document.getElementById('imageWrap').appendChild(img);
+
       }
-    } 
+
+    }
+  }
+  deleteImage(el) {
+    console.log(el)
   }
   render() {
+    const {
+      files,
+      error
+    } = this.state;
+
     return (
       <div>
-        <button type="button" className="btn grey darken-3 btnblock" name="action" onClick={this.addPhoto} ><i className="fas fa-portrait"></i> Add Photo
+        <button type="button" className="btn grey darken-3 btnblock" name="action" onClick={this.addPhoto} hidden={files.length >= 5 ? true : false}><i className="fas fa-portrait"></i> Add Photo
             <i className="material-icons right"></i>
         </button>
+        <div className="center-align red-text">{error}</div>
         <input
           id="file"
           hidden
@@ -41,7 +64,10 @@ export default class AddImages extends Component {
           multiple
           accept="image/x-png,image/gif,image/jpeg"
         />
-        <div id="imageWrap"></div>
+        <ul>
+          {files.map((reptile, index) => <li key={index}> <img width="120px" className="responsive-img" src={reptile} alt="" /> </li>)}
+        </ul>
+
       </div>
     )
   }
